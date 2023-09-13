d583f77961e3e9cd2dabf50f97709eccce239f4c
Rahel Arnold <rahel.arnold@unibas.ch>
2023-09-13 08:17:40 +0200

    implemented a first version of a concrete external descriptor (CLIP)

---
+++ vitrivr-engine-base/src/main/kotlin/org/vitrivr/engine/base/features/external/CLIP/CLIPImage.kt
@@ -0,0 +1,82 @@
+package org.vitrivr.engine.base.features.external.CLIP
+
+import org.vitrivr.engine.base.features.averagecolor.AverageColor
+import org.vitrivr.engine.base.features.averagecolor.AverageColorExtractor
+import org.vitrivr.engine.base.features.external.ExternalImageFeature
+import org.vitrivr.engine.core.model.color.MutableRGBFloatColorContainer
+import org.vitrivr.engine.core.model.color.RGBByteColorContainer
+import org.vitrivr.engine.core.model.color.RGBFloatColorContainer
+import org.vitrivr.engine.core.model.content.ImageContent
+import org.vitrivr.engine.core.model.content.SourcedContent
+import org.vitrivr.engine.core.model.database.descriptor.vector.FloatVectorDescriptor
+import org.vitrivr.engine.core.model.database.retrievable.Ingested
+import org.vitrivr.engine.core.model.metamodel.Schema
+import org.vitrivr.engine.core.model.util.DescriptorList
+import org.vitrivr.engine.core.model.util.toDescriptorList
+import org.vitrivr.engine.core.operators.Operator
+import org.vitrivr.engine.core.operators.ingest.Extractor
+import org.vitrivr.engine.core.operators.retrieve.Retriever
+import org.vitrivr.engine.core.util.extension.getRGBArray
+import java.io.BufferedReader
+import java.io.File
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.*
+import javax.imageio.ImageIO
+import kotlin.reflect.KClass
+
+class CLIPImage(
+    override val analyserName: String = "CLIPImage",
+    override val contentClass: KClass<ImageContent>,
+    override val descriptorClass: KClass<FloatVectorDescriptor>
+) : ExternalImageFeature() {
+
+    val QUERY = "/retrieve/clip/text/"
+
+
+    /**
+     * Performs the [CLIPImage] analysis on the provided [List] of [ImageContent] elements.
+     *
+     * @param content The [List] of [ImageContent] elements.
+     * @return [List] of [FloatVectorDescriptor]s.
+     */
+    override fun analyse(content: Collection<ImageContent>): DescriptorList<FloatVectorDescriptor> = content.map {
+
+        /* Generate descriptor. */
+
+        //TODO request to get feature vector
+        val path = "placeholder.jpg"
+        val out = File(path)
+        ImageIO.write(it.image, "jpg", out)
+
+        val url = URL(DEFAULT_API_ENDPOINT + QUERY + path)
+        val conn: HttpURLConnection = url.openConnection() as HttpURLConnection
+
+        conn.requestMethod = "GET"
+        conn.setRequestProperty("Accept", "text/html")
+
+        if (conn.getResponseCode() !== 200) {
+            throw RuntimeException(
+                "Failed : HTTP error code : " + conn.getResponseCode()
+            )
+        }
+
+        val br = BufferedReader(InputStreamReader(conn.inputStream))
+        val l = br.readLine()
+
+        val vector: List<Float> = l.substring(1, l.length - 1).split(", ").map { it.toFloat() }
+
+
+        FloatVectorDescriptor(UUID.randomUUID(), null, vector, true)
+    }.toDescriptorList()
+
+
+    override fun newExtractor(
+        field: Schema.Field<ImageContent, FloatVectorDescriptor>, input: Operator<Ingested>, persisting: Boolean
+    ): CLIPImageExtractor {
+        require(field.analyser == this) { "" }
+        return CLIPImageExtractor(field, input, persisting)
+    }
+
+}
---
+++ vitrivr-engine-base/src/main/kotlin/org/vitrivr/engine/base/features/external/CLIP/CLIPImageExtractor.kt
@@ -0,0 +1,82 @@
+package org.vitrivr.engine.base.features.external.CLIP
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import org.vitrivr.engine.base.features.external.ExternalImageExtractor
+import org.vitrivr.engine.core.model.content.ImageContent
+import org.vitrivr.engine.core.model.database.descriptor.vector.FloatVectorDescriptor
+import org.vitrivr.engine.core.model.database.retrievable.Ingested
+import org.vitrivr.engine.core.model.database.retrievable.RetrievableWithContent
+import org.vitrivr.engine.core.model.database.retrievable.RetrievableWithDescriptor
+import org.vitrivr.engine.core.model.metamodel.Schema
+import org.vitrivr.engine.core.operators.Operator
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.URL
+import javax.imageio.ImageIO
+
+
+class CLIPImageExtractor(
+    override val field: Schema.Field<ImageContent, FloatVectorDescriptor>,
+    override val input: Operator<Ingested>,
+    override val persisting: Boolean = true,
+) : ExternalImageExtractor() {
+
+    val QUERY = "/retrieve/clip/text/"
+
+
+    // External call API
+    override fun toFlow(scope: CoroutineScope): Flow<Ingested> {
+        val writer = if (this.persisting) {
+            this.field.getWriter()
+        } else {
+            null
+        }
+        return this.input.toFlow(scope).map { retrievable: Ingested ->
+            if (retrievable is RetrievableWithContent) {
+                val content = retrievable.content.filterIsInstance<ImageContent>()
+                // TODO use analyse function to avoid duplicated code!!!
+
+                //val descriptors = this.field.analyser.analyse(content)
+
+                val path = "placeholder.jpg"
+                val out = File(path)
+                ImageIO.write(content[0].image, "jpg", out)
+
+
+                val url = URL(DEFAULT_API_ENDPOINT + QUERY + path)
+                val conn: HttpURLConnection = url.openConnection() as HttpURLConnection
+
+                conn.requestMethod = "GET"
+                conn.setRequestProperty("Accept", "text/html")
+
+                if (conn.getResponseCode() !== 200) {
+                    throw RuntimeException(
+                        "Failed : HTTP error code : " + conn.getResponseCode()
+                    )
+                }
+
+                val br = BufferedReader(InputStreamReader(conn.inputStream))
+                val l = br.readLine()
+
+                val vector: List<Float> = l.substring(1, l.length - 1).split(", ").map { it.toFloat() }
+
+                val descriptor = FloatVectorDescriptor(
+                    retrievableId = retrievable.id, transient = !persisting, vector = vector
+                )
+
+                if (retrievable is RetrievableWithDescriptor.Mutable) {
+                    retrievable.addDescriptor(descriptor)
+                }
+                writer?.add(descriptor)
+            }
+            retrievable
+        }
+            .flowOn(Dispatchers.IO)
+    }
+
+
+}
