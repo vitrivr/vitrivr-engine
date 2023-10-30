60961900641e8273563ce32ab0d766447b7cab48
Luca Rossetto <lucaro@lucaro.ch>
2023-10-30 20:43:36 +0100

    Added mechanism to RetrievableReader to get relationships of Retrievables

--- vitrivr-engine-base/src/main/kotlin/org/vitrivr/engine/base/database/cottontail/retrievable/RetrievableReader.kt
+++ vitrivr-engine-base/src/main/kotlin/org/vitrivr/engine/base/database/cottontail/retrievable/RetrievableReader.kt
@@ -6,13 +6,17 @@ import io.grpc.StatusException
 import org.vitrivr.cottontail.client.language.basics.expression.Column
 import org.vitrivr.cottontail.client.language.basics.expression.List
 import org.vitrivr.cottontail.client.language.basics.expression.Literal
+import org.vitrivr.cottontail.client.language.basics.predicate.And
 import org.vitrivr.cottontail.client.language.basics.predicate.Compare
 import org.vitrivr.cottontail.client.language.dql.Query
 import org.vitrivr.cottontail.core.database.Name
 import org.vitrivr.cottontail.core.values.StringValue
 import org.vitrivr.engine.base.database.cottontail.CottontailConnection
+import org.vitrivr.engine.base.database.cottontail.CottontailConnection.Companion.OBJECT_ID_COLUMN_NAME
+import org.vitrivr.engine.base.database.cottontail.CottontailConnection.Companion.PREDICATE_COLUMN_NAME
 import org.vitrivr.engine.base.database.cottontail.CottontailConnection.Companion.RETRIEVABLE_ID_COLUMN_NAME
 import org.vitrivr.engine.base.database.cottontail.CottontailConnection.Companion.RETRIEVABLE_TYPE_COLUMN_NAME
+import org.vitrivr.engine.base.database.cottontail.CottontailConnection.Companion.SUBJECT_ID_COLUMN_NAME
 import org.vitrivr.engine.core.database.retrievable.RetrievableInitializer
 import org.vitrivr.engine.core.database.retrievable.RetrievableReader
 import org.vitrivr.engine.core.model.retrievable.Retrievable
@@ -28,9 +32,14 @@ private val logger: KLogger = KotlinLogging.logger {}
  * @author Ralph Gasser
  * @version 1.0.0
  */
-internal class RetrievableReader(private val connection: CottontailConnection): RetrievableReader {
+internal class RetrievableReader(private val connection: CottontailConnection) : RetrievableReader {
     /** The [Name.EntityName] for this [RetrievableInitializer]. */
-    private val entityName: Name.EntityName = Name.EntityName(this.connection.schemaName, CottontailConnection.RETRIEVABLE_ENTITY_NAME)
+    private val entityName: Name.EntityName =
+        Name.EntityName(this.connection.schemaName, CottontailConnection.RETRIEVABLE_ENTITY_NAME)
+
+    /** The [Name.EntityName] of the relationship entity. */
+    private val relationshipEntityName: Name.EntityName =
+        Name.EntityName(this.connection.schemaName, CottontailConnection.RELATIONSHIP_ENTITY_NAME)
 
     /**
      * Returns the [Retrievable]s that matches the provided [RetrievableId]
@@ -39,12 +48,21 @@ internal class RetrievableReader(private val connection: CottontailConnection): 
      * @return A [Sequence] of all [Retrievable].
      */
     override fun get(id: RetrievableId): Retrievable? {
-        val query = Query(this.entityName).where(Compare(Column(this.entityName.column(RETRIEVABLE_ID_COLUMN_NAME)), Compare.Operator.EQUAL, Literal(id.toString())))
+        val query = Query(this.entityName).where(
+            Compare(
+                Column(this.entityName.column(RETRIEVABLE_ID_COLUMN_NAME)),
+                Compare.Operator.EQUAL,
+                Literal(id.toString())
+            )
+        )
         return try {
             this.connection.client.query(query).use {
                 if (it.hasNext()) {
                     val tuple = it.next()
-                    val retrievableId = UUID.fromString(tuple.asString(RETRIEVABLE_ID_COLUMN_NAME) ?: throw IllegalArgumentException("The provided tuple is missing the required field '${RETRIEVABLE_ID_COLUMN_NAME}'."))
+                    val retrievableId = UUID.fromString(
+                        tuple.asString(RETRIEVABLE_ID_COLUMN_NAME)
+                            ?: throw IllegalArgumentException("The provided tuple is missing the required field '${RETRIEVABLE_ID_COLUMN_NAME}'.")
+                    )
                     val type = tuple.asString(RETRIEVABLE_TYPE_COLUMN_NAME)
                     Retrieved.Default(retrievableId, type, false) /* TODO: Use UUID type once supported. */
                 } else {
@@ -65,7 +83,13 @@ internal class RetrievableReader(private val connection: CottontailConnection): 
      */
     override fun exists(id: RetrievableId): Boolean {
         val query = Query(this.entityName).exists()
-            .where(Compare(Column(this.entityName.column(RETRIEVABLE_ID_COLUMN_NAME)), Compare.Operator.EQUAL, Literal(id.toString())))
+            .where(
+                Compare(
+                    Column(this.entityName.column(RETRIEVABLE_ID_COLUMN_NAME)),
+                    Compare.Operator.EQUAL,
+                    Literal(id.toString())
+                )
+            )
         return try {
             val result = this.connection.client.query(query)
             result.next().asBoolean(0) ?: false
@@ -82,9 +106,18 @@ internal class RetrievableReader(private val connection: CottontailConnection): 
      * @return A [Sequence] of all [Retrievable].
      */
     override fun getAll(ids: Iterable<RetrievableId>): Sequence<Retrievable> {
-        val query = Query(this.entityName).select("*").where(Compare(Column(Name.ColumnName(RETRIEVABLE_ID_COLUMN_NAME)), Compare.Operator.IN, List(ids.map { StringValue(it.toString()) }.toTypedArray())))
+        val query = Query(this.entityName).select("*").where(
+            Compare(
+                Column(Name.ColumnName(RETRIEVABLE_ID_COLUMN_NAME)),
+                Compare.Operator.IN,
+                List(ids.map { StringValue(it.toString()) }.toTypedArray())
+            )
+        )
         return this.connection.client.query(query).asSequence().map { tuple ->
-            val retrievableId = UUID.fromString(tuple.asString(RETRIEVABLE_ID_COLUMN_NAME) ?: throw IllegalArgumentException("The provided tuple is missing the required field '${RETRIEVABLE_ID_COLUMN_NAME}'."))
+            val retrievableId = UUID.fromString(
+                tuple.asString(RETRIEVABLE_ID_COLUMN_NAME)
+                    ?: throw IllegalArgumentException("The provided tuple is missing the required field '${RETRIEVABLE_ID_COLUMN_NAME}'.")
+            )
             val type = tuple.asString(RETRIEVABLE_TYPE_COLUMN_NAME)
             Retrieved.Default(retrievableId, type, false) /* TODO: Use UUID type once supported. */
         }
@@ -98,7 +131,10 @@ internal class RetrievableReader(private val connection: CottontailConnection): 
     override fun getAll(): Sequence<Retrievable> {
         val query = Query(this.entityName).select("*")
         return this.connection.client.query(query).asSequence().map { tuple ->
-            val retrievableId = UUID.fromString(tuple.asString(RETRIEVABLE_ID_COLUMN_NAME) ?: throw IllegalArgumentException("The provided tuple is missing the required field '${RETRIEVABLE_ID_COLUMN_NAME}'."))
+            val retrievableId = UUID.fromString(
+                tuple.asString(RETRIEVABLE_ID_COLUMN_NAME)
+                    ?: throw IllegalArgumentException("The provided tuple is missing the required field '${RETRIEVABLE_ID_COLUMN_NAME}'.")
+            )
             val type = tuple.asString(RETRIEVABLE_TYPE_COLUMN_NAME)
             Retrieved.Default(retrievableId, type, false) /* TODO: Use UUID type once supported. */
         }
@@ -115,5 +151,69 @@ internal class RetrievableReader(private val connection: CottontailConnection): 
             count = it.asLong(0)!!
         }
         return count
+    }
+
+    /**
+     * Returns connections between [Retrievable]s. Empty collections do not filter.
+     */
+    override fun getConnections(
+        subjectIds: Collection<RetrievableId>,
+        predicates: Collection<String>,
+        objectIds: Collection<RetrievableId>
+    ): Sequence<Triple<RetrievableId, String, RetrievableId>> {
+
+        val filters = listOfNotNull(
+            if (subjectIds.isNotEmpty()) {
+                Compare(
+                    Column(Name.ColumnName(SUBJECT_ID_COLUMN_NAME)),
+                    Compare.Operator.IN,
+                    List(subjectIds.map { StringValue(it.toString()) }.toTypedArray())
+                )
+            } else {
+                null
+            },
+            if (predicates.isNotEmpty()) {
+                Compare(
+                    Column(Name.ColumnName(PREDICATE_COLUMN_NAME)),
+                    Compare.Operator.IN,
+                    List(predicates.map { StringValue(it) }.toTypedArray())
+                )
+            } else {
+                null
+            },
+            if (objectIds.isNotEmpty()) {
+                Compare(
+                    Column(Name.ColumnName(OBJECT_ID_COLUMN_NAME)),
+                    Compare.Operator.IN,
+                    List(objectIds.map { StringValue(it.toString()) }.toTypedArray())
+                )
+            } else {
+                null
+            }
+        )
+
+        val query = Query(this.relationshipEntityName).select("*")
+
+        if (filters.isNotEmpty()) {
+            query.where(
+                when (filters.size) {
+                    3 -> And(filters[0], And(filters[1], filters[2]))
+                    2 -> And(filters[0], filters[1])
+                    else -> filters[0]
+                }
+            )
+        }
+
+        return this.connection.client.query(query).asSequence().map { tuple ->
+            val s = tuple.asString(SUBJECT_ID_COLUMN_NAME)
+                ?: throw IllegalArgumentException("The provided tuple is missing the required field '${SUBJECT_ID_COLUMN_NAME}'.")
+            val p = tuple.asString(PREDICATE_COLUMN_NAME)
+                ?: throw IllegalArgumentException("The provided tuple is missing the required field '${PREDICATE_COLUMN_NAME}'.")
+            val o = tuple.asString(OBJECT_ID_COLUMN_NAME)
+                ?: throw IllegalArgumentException("The provided tuple is missing the required field '${OBJECT_ID_COLUMN_NAME}'.")
+
+            Triple(UUID.fromString(s), p, UUID.fromString(o))
+        }
+
     }
 }
--- vitrivr-engine-core/src/main/kotlin/org/vitrivr/engine/core/database/retrievable/RetrievableReader.kt
+++ vitrivr-engine-core/src/main/kotlin/org/vitrivr/engine/core/database/retrievable/RetrievableReader.kt
@@ -2,6 +2,7 @@ package org.vitrivr.engine.core.database.retrievable
 
 import org.vitrivr.engine.core.database.Reader
 import org.vitrivr.engine.core.model.retrievable.Retrievable
+import org.vitrivr.engine.core.model.retrievable.RetrievableId
 
 /**
  * A [RetrievableWriter] is an extension of a [Retrievable] for [Retrievable]s.
@@ -10,4 +11,12 @@ import org.vitrivr.engine.core.model.retrievable.Retrievable
  * @author Ralph Gasser
  * @version 1.0.0
  */
-interface RetrievableReader: Reader<Retrievable>
+interface RetrievableReader : Reader<Retrievable> {
+
+    fun getConnections(
+        subjectIds: Collection<RetrievableId>,
+        predicates: Collection<String>,
+        objectIds: Collection<RetrievableId>
+    ): Sequence<Triple<RetrievableId, String, RetrievableId>>
+
+}
