8df9ae60a52cd2dccd5f642d0196ea12b2b734d9
Luca Rossetto <lucaro@lucaro.ch>
2023-11-18 18:03:32 +0100

    Added type field to QueryResultRetrievable

--- vitrivr-engine-query/src/main/kotlin/org/vitrivr/engine/query/model/api/result/QueryResultRetrievable.kt
+++ vitrivr-engine-query/src/main/kotlin/org/vitrivr/engine/query/model/api/result/QueryResultRetrievable.kt
@@ -6,10 +6,11 @@ import org.vitrivr.engine.core.model.retrievable.Retrieved
 typealias RetrievableIdString = String
 
 @Serializable
-data class QueryResultRetrievable(val id: RetrievableIdString, val score: Float, val parts: MutableList<RetrievableIdString>) {
+data class QueryResultRetrievable(val id: RetrievableIdString, val score: Float, val type: String, val parts: MutableList<RetrievableIdString>) {
     constructor(retrieved: Retrieved) : this(
         retrieved.id.toString(),
         if (retrieved is Retrieved.RetrievedWithScore) retrieved.score else 0f,
+        retrieved.type ?: "",
         mutableListOf()
     )
 }
