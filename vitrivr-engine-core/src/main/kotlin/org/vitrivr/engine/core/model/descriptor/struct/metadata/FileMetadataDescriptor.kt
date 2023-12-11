a39b330b59da225bbc1e6e0a9ecbff2fd32f0f4e
Luca Rossetto <lucaro@lucaro.ch>
2023-12-11 21:41:04 +0100

    Fixed parameter ordering in FileMetadataDescriptor

--- vitrivr-engine-core/src/main/kotlin/org/vitrivr/engine/core/model/descriptor/struct/metadata/FileMetadataDescriptor.kt
+++ vitrivr-engine-core/src/main/kotlin/org/vitrivr/engine/core/model/descriptor/struct/metadata/FileMetadataDescriptor.kt
@@ -13,8 +13,8 @@ import org.vitrivr.engine.core.model.retrievable.RetrievableId
  * @version 1.0.0
  */
 data class FileMetadataDescriptor(
-    override val id: DescriptorId,
     override val retrievableId: RetrievableId,
+    override val id: DescriptorId,
     val path: String,
     val size: Long,
     override val transient: Boolean = false
