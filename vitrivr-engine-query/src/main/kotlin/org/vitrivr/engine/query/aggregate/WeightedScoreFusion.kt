8f5bb597c9a61fb42ddeb3a855b78644916efec2
Luca Rossetto <lucaro@lucaro.ch>
2023-12-13 20:43:04 +0100

    Added first version of WeightedScoreFusion

---
+++ vitrivr-engine-query/src/main/kotlin/org/vitrivr/engine/query/aggregate/WeightedScoreFusion.kt
@@ -0,0 +1,69 @@
+package org.vitrivr.engine.query.aggregate
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.toList
+import org.vitrivr.engine.core.model.retrievable.Retrievable
+import org.vitrivr.engine.core.model.retrievable.RetrievableId
+import org.vitrivr.engine.core.model.retrievable.Retrieved
+import org.vitrivr.engine.core.operators.Operator
+import org.vitrivr.engine.core.operators.retrieve.Aggregator
+
+class WeightedScoreFusion(
+    override val inputs: List<Operator<Retrieved>>,
+    weights: List<Float>
+) : Aggregator<Retrieved, Retrieved> {
+
+    private val weights: List<Float>
+    init {
+        this.weights = when {
+            weights.size > inputs.size -> weights.subList(0, inputs.size - 1)
+            weights.size < inputs.size -> weights + List(inputs.size - weights.size) {1f}
+            else -> weights
+        }
+    }
+
+    private val weightsSum = this.weights.sum()
+
+    override fun toFlow(scope: CoroutineScope): Flow<Retrieved> = flow {
+
+        val inputs = inputs.map { it.toFlow(scope).toList() }
+
+        val scoreMap = mutableMapOf<RetrievableId, MutableList<Pair<Int, Retrievable>>>()
+
+        for ((index, retrieveds) in inputs.withIndex()) {
+
+            for (retrieved in retrieveds) {
+
+                if (!scoreMap.containsKey(retrieved.id)) {
+                    scoreMap[retrieved.id] = mutableListOf()
+                }
+
+                scoreMap[retrieved.id]!!.add(index to retrieved)
+
+            }
+
+        }
+
+        for((_, retrieveds) in scoreMap) {
+
+            val score = retrieveds.map { ((it.second as? Retrieved.RetrievedWithScore)?.score ?: 0f) * weights[it.first] }.sum() / weightsSum
+
+            val first = retrieveds.first().second
+
+
+            //TODO better merging with type/attribute preservation
+            val retrieved = Retrieved.WithScore(
+                first.id,
+                first.type,
+                score,
+                false
+            )
+
+            emit(retrieved)
+
+        }
+
+    }
+}
---
+++ vitrivr-engine-query/src/main/kotlin/org/vitrivr/engine/query/aggregate/WeightedScoreFusionFactory.kt
@@ -0,0 +1,18 @@
+package org.vitrivr.engine.query.aggregate
+
+import org.vitrivr.engine.core.model.metamodel.Schema
+import org.vitrivr.engine.core.model.retrievable.Retrieved
+import org.vitrivr.engine.core.operators.Operator
+import org.vitrivr.engine.core.operators.retrieve.Aggregator
+import org.vitrivr.engine.core.operators.retrieve.AggregatorFactory
+
+class WeightedScoreFusionFactory : AggregatorFactory<Retrieved, Retrieved> {
+    override fun newAggregator(
+        inputs: List<Operator<Retrieved>>,
+        schema: Schema,
+        properties: Map<String, String>
+    ): Aggregator<Retrieved, Retrieved> {
+        val weights = properties["weights"]?.split(",")?.mapNotNull { s -> s.trim().toFloatOrNull() } ?: emptyList()
+        return WeightedScoreFusion(inputs, weights)
+    }
+}
