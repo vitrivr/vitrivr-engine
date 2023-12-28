16d8f189fff1acf9934a97fde71eb057b76b4654
Ralph Gasser <rg@pontius.ch>
2023-12-28 10:49:27 +0100

    Implementation of FirstContentAggregator, LastContentAggregator and CenterContentAggregator is now agnostic to the type of content used.
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- vitrivr-engine-index/src/main/kotlin/org/vitrivr/engine/index/aggregators/CenterContentAggregator.kt
+++ vitrivr-engine-index/src/main/kotlin/org/vitrivr/engine/index/aggregators/CenterContentAggregator.kt
@@ -1,10 +1,7 @@
 package org.vitrivr.engine.index.aggregators
 
 import org.vitrivr.engine.core.context.IndexContext
-import org.vitrivr.engine.core.model.content.element.AudioContent
 import org.vitrivr.engine.core.model.content.element.ContentElement
-import org.vitrivr.engine.core.model.content.element.ImageContent
-import org.vitrivr.engine.core.model.content.element.TextContent
 import org.vitrivr.engine.core.model.retrievable.Retrievable
 import org.vitrivr.engine.core.operators.Operator
 import org.vitrivr.engine.core.operators.ingest.Aggregator
@@ -14,7 +11,7 @@ import org.vitrivr.engine.core.operators.ingest.Segmenter
 /**
  * A [Aggregator] that returns the middle [ContentElement] of each type.
  *
- * @author Luca Rossetto
+ * @author
  * @version 1.0.0
  */
 class CenterContentAggregator : AggregatorFactory {
@@ -33,15 +30,14 @@ class CenterContentAggregator : AggregatorFactory {
      * The [Instance] returns by the [AggregatorFactory]
      */
     private class Instance(override val input: Operator<Retrievable>, context: IndexContext) : AbstractAggregator(input, context) {
-        override fun aggregate(content: List<ContentElement<*>>): List<ContentElement<*>> {
-            val imageContent = content.filterIsInstance<ImageContent>()
-            val audioContent = content.filterIsInstance<AudioContent>()
-            val textContent = content.filterIsInstance<TextContent>()
-            return listOfNotNull(
-                imageContent.let { if (it.isNotEmpty()) it[Math.floorDiv(it.size, 2)] else null },
-                audioContent.let { if (it.isNotEmpty()) it[Math.floorDiv(it.size, 2)] else null },
-                textContent.let { if (it.isNotEmpty()) it[Math.floorDiv(it.size, 2)] else null }
-            )
+        override fun aggregate(content: List<ContentElement<*>>): List<ContentElement<*>> = content.groupBy {
+            it::class
+        }.mapNotNull { (_, elements) ->
+            if (elements.isNotEmpty()) {
+                elements[Math.floorDiv(elements.size, 2)]
+            } else {
+                null
+            }
         }
     }
 }
--- vitrivr-engine-index/src/main/kotlin/org/vitrivr/engine/index/aggregators/FirstContentAggregator.kt
+++ vitrivr-engine-index/src/main/kotlin/org/vitrivr/engine/index/aggregators/FirstContentAggregator.kt
@@ -1,10 +1,7 @@
 package org.vitrivr.engine.index.aggregators
 
 import org.vitrivr.engine.core.context.IndexContext
-import org.vitrivr.engine.core.model.content.element.AudioContent
 import org.vitrivr.engine.core.model.content.element.ContentElement
-import org.vitrivr.engine.core.model.content.element.ImageContent
-import org.vitrivr.engine.core.model.content.element.TextContent
 import org.vitrivr.engine.core.model.retrievable.Retrievable
 import org.vitrivr.engine.core.operators.Operator
 import org.vitrivr.engine.core.operators.ingest.Aggregator
@@ -33,11 +30,6 @@ class FirstContentAggregator : AggregatorFactory {
      * The [Instance] returns by the [AggregatorFactory]
      */
     private class Instance(override val input: Operator<Retrievable>, context: IndexContext) : AbstractAggregator(input, context) {
-        override fun aggregate(content: List<ContentElement<*>>): List<ContentElement<*>> {
-            val firstImage = content.firstOrNull { it is ImageContent } as? ImageContent
-            val firstAudio = content.firstOrNull { it is AudioContent } as? AudioContent
-            val firstText = content.firstOrNull { it is TextContent } as? TextContent
-            return listOfNotNull(firstImage, firstAudio, firstText)
-        }
+        override fun aggregate(content: List<ContentElement<*>>): List<ContentElement<*>> = content.groupBy { it::class }.mapNotNull { (_, elements) -> elements.firstOrNull() }
     }
 }
