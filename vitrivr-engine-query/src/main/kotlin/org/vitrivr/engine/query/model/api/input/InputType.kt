ec5dca69e0976bef84162c3b8abd51fdea21e9a4
Raphael <raphael.waltenspuel@unibas.ch>
2024-03-22 08:48:10 +0100

    Added SerialName

--- vitrivr-engine-query/src/main/kotlin/org/vitrivr/engine/query/model/api/input/InputType.kt
+++ vitrivr-engine-query/src/main/kotlin/org/vitrivr/engine/query/model/api/input/InputType.kt
@@ -1,10 +1,16 @@
 package org.vitrivr.engine.query.model.api.input
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 enum class InputType {
-
+    @SerialName("TEXT")
     TEXT,
+    @SerialName("IMAGE")
     IMAGE,
+    @SerialName("VECTOR")
     VECTOR,
+    @SerialName("ID")
     ID
-
 }
