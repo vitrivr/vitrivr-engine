278b79fb2c3a1fb7fe0524eec35587fef1b85634
Ralph Gasser <ralph.gasser@unibas.ch>
2024-03-27 11:52:16 +0100

    FieldLookup now appends descriptor instead of a property map.

--- vitrivr-engine-query/src/main/kotlin/org/vitrivr/engine/query/transform/FieldLookup.kt
+++ vitrivr-engine-query/src/main/kotlin/org/vitrivr/engine/query/transform/FieldLookup.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.toList
 import org.vitrivr.engine.core.database.descriptor.DescriptorReader
 import org.vitrivr.engine.core.model.retrievable.Retrieved
+import org.vitrivr.engine.core.model.retrievable.attributes.DescriptorAttribute
 import org.vitrivr.engine.core.model.retrievable.attributes.PropertyAttribute
 import org.vitrivr.engine.core.operators.Operator
 import org.vitrivr.engine.core.operators.retrieve.Transformer
@@ -13,39 +14,23 @@ import org.vitrivr.engine.core.operators.retrieve.Transformer
 /**
  * Appends stringified key-value pairs to a [Retrieved] based on the values of a [Field], if available
  */
-class FieldLookup(
-    override val input: Operator<Retrieved>,
-    private val reader: DescriptorReader<*>,
-    private val keys: Collection<String>
-
-) : Transformer {
+class FieldLookup(override val input: Operator<Retrieved>, private val reader: DescriptorReader<*>) : Transformer {
     override fun toFlow(scope: CoroutineScope): Flow<Retrieved> = flow {
-
+        /* Parse input IDs.*/
         val inputRetrieved = input.toFlow(scope).toList()
-
         val ids = inputRetrieved.map { it.id }.toSet()
-
         if (ids.isEmpty()) {
             return@flow
         }
 
-        val descriptors =
-            reader.getAllBy(ids, "retrievableId").filter { it.retrievableId != null }.associateBy { it.retrievableId!! }
-
+        /* Perform lookup. */
+        val descriptors = reader.getAllBy(ids, "retrievableId").filter { it.retrievableId != null }.associateBy { it.retrievableId!! }
         inputRetrieved.forEach { retrieved ->
-
             val descriptor = descriptors[retrieved.id]
-
             if (descriptor != null) {
-                val values = descriptor.values().toMap().mapValues { it.toString() }
-                val attribute = PropertyAttribute(values)
-                retrieved.addAttribute(attribute)
+                retrieved.addAttribute(DescriptorAttribute(descriptor))
             }
-
             emit(retrieved)
-
         }
-
     }
-
 }
