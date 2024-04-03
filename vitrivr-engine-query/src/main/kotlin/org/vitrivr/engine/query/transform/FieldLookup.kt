53f9b0ddad8641a9fac9ffe02073956df17638ed
Ralph Gasser <ralph.gasser@unibas.ch>
2024-04-03 10:24:58 +0200

    Minor optimisation.

--- vitrivr-engine-query/src/main/kotlin/org/vitrivr/engine/query/transform/FieldLookup.kt
+++ vitrivr-engine-query/src/main/kotlin/org/vitrivr/engine/query/transform/FieldLookup.kt
@@ -5,26 +5,33 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.toList
 import org.vitrivr.engine.core.database.descriptor.DescriptorReader
+import org.vitrivr.engine.core.model.metamodel.Schema
 import org.vitrivr.engine.core.model.retrievable.Retrieved
 import org.vitrivr.engine.core.model.retrievable.attributes.DescriptorAttribute
-import org.vitrivr.engine.core.model.retrievable.attributes.PropertyAttribute
 import org.vitrivr.engine.core.operators.Operator
 import org.vitrivr.engine.core.operators.retrieve.Transformer
 
 /**
- * Appends stringified key-value pairs to a [Retrieved] based on the values of a [Field], if available
+ * Appends [DescriptorAttribute] to a [Retrieved] based on the values of a [Schema.Field], if available.
+ *
+ * @version 1.1.0
+ * @author Luca Rossetto
+ * @author Ralph Gasser
  */
 class FieldLookup(override val input: Operator<Retrieved>, private val reader: DescriptorReader<*>) : Transformer {
     override fun toFlow(scope: CoroutineScope): Flow<Retrieved> = flow {
         /* Parse input IDs.*/
         val inputRetrieved = input.toFlow(scope).toList()
+
+        /* Fetch entries for the provided IDs. */
         val ids = inputRetrieved.map { it.id }.toSet()
-        if (ids.isEmpty()) {
-            return@flow
+        val descriptors = if (ids.isEmpty()) {
+            emptyMap()
+        } else {
+            this@FieldLookup.reader.getAllBy(ids, "retrievableId").filter { it.retrievableId != null }.associateBy { it.retrievableId!! }
         }
 
-        /* Perform lookup. */
-        val descriptors = reader.getAllBy(ids, "retrievableId").filter { it.retrievableId != null }.associateBy { it.retrievableId!! }
+        /* Emit retrievable with added attribute. */
         inputRetrieved.forEach { retrieved ->
             val descriptor = descriptors[retrieved.id]
             if (descriptor != null) {
