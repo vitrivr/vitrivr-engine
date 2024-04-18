177a372b60d18bf8815111a9aaf5acf1d25b1112
Laura Rettig <rettigx@gmail.com>
2024-04-18 15:15:22 +0200

    Wrote minimal skeletons for ExifMetdata functionality and defined a reduced schema for testing.

--- vitrivr-engine-core/src/main/kotlin/org/vitrivr/engine/core/features/metadata/source/exif/ExifMetadata.kt
+++ vitrivr-engine-core/src/main/kotlin/org/vitrivr/engine/core/features/metadata/source/exif/ExifMetadata.kt
@@ -8,6 +8,8 @@ import org.vitrivr.engine.core.model.content.element.ContentElement
 import org.vitrivr.engine.core.model.descriptor.struct.metadata.source.MapStructDescriptor
 import org.vitrivr.engine.core.model.metamodel.Analyser
 import org.vitrivr.engine.core.model.metamodel.Schema
+import org.vitrivr.engine.core.model.query.Query
+import org.vitrivr.engine.core.model.query.bool.SimpleBooleanQuery
 import org.vitrivr.engine.core.model.retrievable.Retrievable
 import org.vitrivr.engine.core.operators.Operator
 import org.vitrivr.engine.core.operators.retrieve.Retriever
@@ -18,8 +20,13 @@ private val logger: KLogger = KotlinLogging.logger {}
 class ExifMetadata : Analyser<ContentElement<*>, MapStructDescriptor> {
     override val contentClasses = setOf(ContentElement::class)
     override val descriptorClass = MapStructDescriptor::class
+    override fun prototype(field: Schema.Field<*, *>) = MapStructDescriptor.PROTOTYPE
 
-    override fun prototype() = MapStructDescriptor(UUID.randomUUID(), UUID.randomUUID(), emptyMap())
+    override fun newRetrieverForQuery(field: Schema.Field<ContentElement<*>, MapStructDescriptor>, query: Query, context: QueryContext): Retriever<ContentElement<*>, MapStructDescriptor> {
+        require(field.analyser == this) { "Field type is incompatible with analyser. This is a programmer's error!" }
+        require(query is SimpleBooleanQuery<*>) { "Query is not a Query." }
+        return ExifMetadataRetriever(field, query, context)
+    }
 
     override fun newExtractor(field: Schema.Field<ContentElement<*>, MapStructDescriptor>, input: Operator<Retrievable>, context: IndexContext, persisting: Boolean, parameters: Map<String, Any>): ExifMetadataExtractor {
         require(field.analyser == this) { "Field type is incompatible with analyser. This is a programmer's error!" }
@@ -28,16 +35,6 @@ class ExifMetadata : Analyser<ContentElement<*>, MapStructDescriptor> {
     }
 
     override fun newRetrieverForContent(field: Schema.Field<ContentElement<*>, MapStructDescriptor>, content: Collection<ContentElement<*>>, context: QueryContext): ExifMetadataRetriever {
-        require(field.analyser == this) { "Field type is incompatible with analyser. This is a programmer's error!" }
-        return ExifMetadataRetriever(field, context)
-    }
-
-    override fun newRetrieverForDescriptors(
-        field: Schema.Field<ContentElement<*>, MapStructDescriptor>,
-        descriptors: Collection<MapStructDescriptor>,
-        context: QueryContext
-    ): Retriever<ContentElement<*>, MapStructDescriptor> {
-        require(field.analyser == this) { "Field type is incompatible with analyser. This is a programmer's error!" }
-        return ExifMetadataRetriever(field, context)
+        throw UnsupportedOperationException("ExifMetadata does not support the creation of a Retriever instance from content.")
     }
 }
--- vitrivr-engine-core/src/main/kotlin/org/vitrivr/engine/core/features/metadata/source/exif/ExifMetadataExtractor.kt
+++ vitrivr-engine-core/src/main/kotlin/org/vitrivr/engine/core/features/metadata/source/exif/ExifMetadataExtractor.kt
@@ -1,4 +1,22 @@
 package org.vitrivr.engine.core.features.metadata.source.exif
 
-class ExifMetadataExtractor {
+import org.vitrivr.engine.core.features.AbstractExtractor
+import org.vitrivr.engine.core.model.content.element.ContentElement
+import org.vitrivr.engine.core.model.descriptor.struct.metadata.source.MapStructDescriptor
+import org.vitrivr.engine.core.model.metamodel.Schema
+import org.vitrivr.engine.core.model.retrievable.Retrievable
+import org.vitrivr.engine.core.operators.Operator
+
+class ExifMetadataExtractor(
+    input: Operator<Retrievable>,
+    field: Schema.Field<ContentElement<*>, MapStructDescriptor>,
+    persisting: Boolean = true
+) : AbstractExtractor<ContentElement<*>, MapStructDescriptor>(input, field, persisting, bufferSize = 1) {
+    override fun matches(retrievable: Retrievable): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun extract(retrievable: Retrievable): List<MapStructDescriptor> {
+        TODO("Not yet implemented")
+    }
 }
