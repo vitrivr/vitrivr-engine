d937674c14dcc14b011978f604be1e602a36ffe8
Ralph Gasser <rg@pontius.ch>
2024-04-28 10:42:01 +0200

    Fixes UnitTest.
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- vitrivr-engine-core/src/test/kotlin/org/vitrivr/engine/core/config/ingest/IngestionPipelineBuilderTest.kt
+++ vitrivr-engine-core/src/test/kotlin/org/vitrivr/engine/core/config/ingest/IngestionPipelineBuilderTest.kt
@@ -3,10 +3,12 @@ package org.vitrivr.engine.core.config.ingest
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.fail
-import org.vitrivr.engine.core.config.ContextConfig
 import org.vitrivr.engine.core.config.ingest.operation.OperationConfig
-import org.vitrivr.engine.core.config.ingest.operator.*
-import org.vitrivr.engine.core.model.metamodel.MockSchema
+import org.vitrivr.engine.core.config.ingest.operator.OperatorConfig
+import org.vitrivr.engine.core.context.IngestionContextConfig
+import org.vitrivr.engine.core.database.MockConnection
+import org.vitrivr.engine.core.model.metamodel.Schema
+import org.vitrivr.engine.core.resolver.impl.DiskResolver
 
 class IngestionPipelineBuilderTest {
 
@@ -17,51 +19,38 @@ class IngestionPipelineBuilderTest {
             /* Building the config */
             val config = IngestionConfig(
                 "test-schema",
-                context = ContextConfig(
+                context = IngestionContextConfig(
                     "InMemoryContentFactory",
-                    "DiskResolver",
-                    mapOf("location" to "./thumbnails/testing")
+                    "disk",
+                    global = mapOf("location" to "./thumbnails/testing")
                 ),
-                //mapOf("C2" to mapOf("maxSideResolution" to "350"))
-                enumerator = EnumeratorConfig(
-                    "FileSystemEnumerator",
-                    mapOf(
-                        "path" to "./testing/source",
-                        "mediaTypes" to "IMAGE;VIDEO",
-                        "depth" to "1"
-                    )
-                ),
-                decoder = DecoderConfig(factory = "ImageDecoder"),
                 operators = mapOf(
-                    "A" to SegmenterConfig(factory = "PassThroughSegmenter"),
-                    "B1" to AggregatorConfig(factory = "AllContentAggregator"),
-                    "B2" to AggregatorConfig(factory = "AllContentAggregator"),
-                    "C1" to ExtractorConfig(fieldName = "averagecolor"),
-                    "C2" to ExporterConfig(
-                        exporterName = "thumbnail",
-                        parameters = mapOf("maxSideResolution" to "350", "mimeType" to "JPG")
-                    ),
-                    "D1" to ExtractorConfig(fieldName = "file")
+                    "enumerator" to OperatorConfig.Enumerator("FileSystemEnumerator"),
+                    "decoder" to OperatorConfig.Decoder(factory = "ImageDecoder"),
+                    "segmenter" to OperatorConfig.Transformer(factory = "PassThroughSegmenter"),
+                    "averagecolor" to OperatorConfig.Extractor(fieldName = "averagecolor"),
+                    "thumbnail" to OperatorConfig.Exporter(exporterName = "thumbnail"),
+                    "file" to OperatorConfig.Extractor(fieldName = "file"),
+                    "filter" to OperatorConfig.Transformer(factory = "TypeFilterTransformer")
                 ),
                 operations = mapOf(
-                    "stage1" to OperationConfig(
-                        "A",
-                        listOf("stage2-1", "stage2-2")
-                    ),
-                    "stage2-1" to OperationConfig("B1", listOf("stage3-1")),
-                    "stage2-2" to OperationConfig("B2", listOf("stage3-2")),
-                    "stage3-1" to OperationConfig("C1", listOf("stage4")),
-                    "stage3-2" to OperationConfig("C2"),
-                    "stage4" to OperationConfig("D1")
-                )
+                    "enumerator" to OperationConfig("enumerator"),
+                    "decoder" to OperationConfig("decoder", inputs = listOf("enumerator")),
+                    "segmenter" to OperationConfig("segmenter", inputs = listOf("decoder")),
+                    "averagecolor" to OperationConfig("averagecolor", inputs = listOf("segmenter")),
+                    "thumbnail" to OperationConfig("thumbnail", inputs = listOf("segmenter")),
+                    "filter" to OperationConfig("filter", inputs = listOf("averagecolor", "thumbnail")),
+                    "file" to OperationConfig("file", inputs = listOf("filter"))
+                ),
+                output = listOf("file")
             )
 
-            val mockSchema = MockSchema()
+            val mockSchema = Schema("test-schema", MockConnection("test-schema"))
+            mockSchema.addResolver("disk", DiskResolver().newResolver(mockSchema, mapOf("location" to "./thumbnails/testing")))
             val testSubject = IngestionPipelineBuilder(mockSchema, config)
             testSubject.parseOperations()
         } catch (e: Exception) {
             fail(e)
         }
-
     }
 }
