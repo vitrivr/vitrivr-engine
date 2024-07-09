375e3d953b7cfd82cde89176ac70652a06bd447e
Ralph Gasser <ralph.gasser@unibas.ch>
2024-07-09 09:25:23 +0200

    StringConnection now indicates that it does not support transaction when using the respective method.

--- vitrivr-engine-module-features/src/main/kotlin/org/vitrivr/engine/module/features/database/string/StringConnection.kt
+++ vitrivr-engine-module-features/src/main/kotlin/org/vitrivr/engine/module/features/database/string/StringConnection.kt
@@ -1,5 +1,7 @@
 package org.vitrivr.engine.module.features.database.string
 
+import io.github.oshai.kotlinlogging.KLogger
+import io.github.oshai.kotlinlogging.KotlinLogging.logger
 import org.vitrivr.engine.core.database.AbstractConnection
 import org.vitrivr.engine.core.database.retrievable.NoRetrievableInitializer
 import org.vitrivr.engine.core.database.retrievable.RetrievableInitializer
@@ -9,7 +11,19 @@ import org.vitrivr.engine.core.model.Persistable
 import org.vitrivr.engine.module.features.database.string.writer.StringRetrievableWriter
 
 
+
+/** Defines [KLogger] of the class. */
+internal val LOGGER: KLogger = logger("org.vitrivr.engine.module.database.string.StringConnection")
+
 class StringConnection(override val provider: StringConnectionProvider, schemaName: String, internal val stringify: (Persistable) -> String) : AbstractConnection(schemaName, provider) {
+
+    /**
+     * [StringConnection] does not support transactions.
+     */
+    override fun <T> withTransaction(action: (Unit) -> T): T {
+        LOGGER.warn { "Transactions are not supported by the StringConnection." }
+        return action.invoke(Unit)
+    }
 
     override fun getRetrievableInitializer(): RetrievableInitializer = NoRetrievableInitializer()
 
