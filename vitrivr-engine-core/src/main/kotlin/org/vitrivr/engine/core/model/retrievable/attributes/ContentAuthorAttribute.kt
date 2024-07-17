1c83e7e3a953e4182cf599900e73d4683496a062
faberf <fynn.f.faber@proton.me>
2024-07-17 15:35:24 +0200

    bidirectional content author maps

--- vitrivr-engine-core/src/main/kotlin/org/vitrivr/engine/core/model/retrievable/attributes/ContentAuthorAttribute.kt
+++ vitrivr-engine-core/src/main/kotlin/org/vitrivr/engine/core/model/retrievable/attributes/ContentAuthorAttribute.kt
@@ -4,20 +4,32 @@ import java.util.*
 import kotlin.collections.HashMap
 
 class ContentAuthorAttribute private constructor(
-    private val authorMap: HashMap<UUID, HashSet<String>>
+    private val authorMap: HashMap<UUID, HashSet<String>>,
+    private val contentMap: HashMap<String, HashSet<UUID>>
 ) : MergingRetrievableAttribute {
 
-    constructor(contentId: UUID, author: String) : this(hashMapOf(contentId to hashSetOf(author)))
+    constructor(contentId: UUID, author: String) : this(hashMapOf(contentId to hashSetOf(author)), hashMapOf(author to hashSetOf(contentId)))
 
     override fun merge(other: MergingRetrievableAttribute): MergingRetrievableAttribute {
-        val otherMap = (other as ContentAuthorAttribute).authorMap
-        for ((contentId, authors) in otherMap) {
+        val otherAuthorMap = (other as ContentAuthorAttribute).authorMap
+        for ((contentId, authors) in otherAuthorMap) {
             authorMap.computeIfAbsent(contentId) { hashSetOf() }.addAll(authors)
         }
-        return ContentAuthorAttribute(authorMap)
+
+        val otherContentMap = other.contentMap
+        for ((author, contentIds) in otherContentMap) {
+            contentMap.computeIfAbsent(author) { hashSetOf() }.addAll(contentIds)
+        }
+
+        return ContentAuthorAttribute(authorMap, contentMap)
     }
 
     fun getAuthors(contentId: UUID): Set<String> {
         return authorMap[contentId] ?: emptySet()
     }
+
+    fun getContentIds(author: String): Set<UUID> {
+        return contentMap[author] ?: emptySet()
+    }
+
 }
