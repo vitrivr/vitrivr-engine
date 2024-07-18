01d0ee3276a9bf4d9c7cffa3e0272e487ef46f7a
Laura Rettig <rettigx@gmail.com>
2024-07-18 12:53:15 +0200

    Updated ContentMergingTransformer to transform content to content rather than descriptor to content.

--- vitrivr-engine-index/src/main/kotlin/org/vitrivr/engine/index/transform/ContentMergingTransformer.kt
+++ vitrivr-engine-index/src/main/kotlin/org/vitrivr/engine/index/transform/ContentMergingTransformer.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import org.vitrivr.engine.core.context.Context
 import org.vitrivr.engine.core.context.IndexContext
+import org.vitrivr.engine.core.model.content.ContentType
 import org.vitrivr.engine.core.model.content.factory.ContentFactory
 import org.vitrivr.engine.core.model.descriptor.scalar.StringDescriptor
 import org.vitrivr.engine.core.model.retrievable.Retrievable
@@ -13,6 +14,8 @@ import org.vitrivr.engine.core.model.retrievable.attributes.ContentAuthorAttribu
 import org.vitrivr.engine.core.operators.Operator
 import org.vitrivr.engine.core.operators.general.Transformer
 import org.vitrivr.engine.core.operators.general.TransformerFactory
+import java.time.Year
+import javax.swing.text.AbstractDocument.Content
 
 private val logger = KotlinLogging.logger {}
 
@@ -26,28 +29,35 @@ class ContentMergingTransformer : TransformerFactory {
     override fun newTransformer(name: String, input: Operator<out Retrievable>, context: Context): Transformer {
         val contentFields = context[name, "contentFields"]?.split(",") ?: throw IllegalArgumentException("The content merging transformer requires a list of content fields.")
         val template = context[name, "template"] ?: throw IllegalArgumentException("The content merging transformer requires a template.")
+        val defaultValue = context[name, "defaultValue"] ?: ""
         return Instance(
             input = input,
             contentFactory = (context as IndexContext).contentFactory,
             contentFields = contentFields,
             template = template,
+            defaultValue = defaultValue,
             name = name
         )
     }
 
-    private class Instance(override val input: Operator<out Retrievable>, val contentFactory: ContentFactory, val contentFields: List<String>, val template: String, val name: String) : Transformer {
+    private class Instance(override val input: Operator<out Retrievable>, val contentFactory: ContentFactory, val contentFields: List<String>, val template: String, val defaultValue: String, val name: String) : Transformer {
         override fun toFlow(scope: CoroutineScope): Flow<Retrievable> = flow {
             input.toFlow(scope).collect { retrievable: Retrievable ->
                 var mergedContent = template
 
                 contentFields.forEach { fieldName ->
                     val placeholder = "\$$fieldName"
-                    val contentText = retrievable.descriptors.find {
-                        it.field?.fieldName == fieldName
-                    }?.let { descriptor ->
-                        if (descriptor is StringDescriptor) descriptor.value.value else ""
-                    } ?: ""
-                    mergedContent = mergedContent.replace(placeholder, contentText)
+                    val contentIds = retrievable.filteredAttribute(ContentAuthorAttribute::class.java)?.getContentIds(fieldName)
+                    val fieldContent = StringBuilder()
+                    contentIds?.forEach{ id ->
+                        retrievable.content.find {
+                            it.id == id && it.type == ContentType.TEXT
+                        }?.content?.let {
+                            fieldContent.append(it)
+                        }
+                    }
+                    val finalContent = if (fieldContent.isEmpty()) defaultValue else fieldContent.toString()
+                    mergedContent = mergedContent.replace(placeholder, finalContent)
                 }
 
                 if (mergedContent.isNotBlank()) {
