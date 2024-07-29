a58428a2dcc802bb209b60317bbba6b21f2e2910
faberf <fynn.f.faber@proton.me>
2024-07-29 15:02:18 +0200

    fixed return type of schemalist and implemented tests

--- vitrivr-engine-server/src/main/kotlin/org/vitrivr/engine/server/api/rest/KotlinxJsonMapper.kt
+++ vitrivr-engine-server/src/main/kotlin/org/vitrivr/engine/server/api/rest/KotlinxJsonMapper.kt
@@ -1,55 +1,53 @@
-import io.javalin.json.JsonMapper
-import kotlinx.serialization.*
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.modules.SerializersModule
-import kotlinx.serialization.serializer
-import kotlin.reflect.KType
-import java.lang.reflect.ParameterizedType
-import java.lang.reflect.Type
-import kotlin.reflect.KTypeProjection
-import kotlin.reflect.full.createType
+    import io.javalin.json.JsonMapper
+    import kotlinx.serialization.*
+    import kotlinx.serialization.json.Json
+    import kotlinx.serialization.serializer
+    import kotlin.reflect.KType
+    import java.lang.reflect.ParameterizedType
+    import java.lang.reflect.Type
+    import kotlin.reflect.KTypeProjection
+    import kotlin.reflect.full.createType
 
-object KotlinxJsonMapper : JsonMapper {
+    object KotlinxJsonMapper : JsonMapper {
 
-    private val json = Json {
-        serializersModule = SerializersModule { }
-        isLenient = true
-        ignoreUnknownKeys = true
-    }
-
-    override fun <T : Any> fromJsonString(jsonString: String, targetType: Type): T {
-        return try {
-            val kType : KType = targetType.toKType()
-            val serializer = json.serializersModule.serializer(kType)
-            json.decodeFromString(serializer, jsonString) as T
-        } catch (e: SerializationException) {
-            throw Exception("Error while deserializing JSON: ${e.message}")
-        } catch (e: IllegalStateException) {
-            throw Exception("Error state: ${e.message}")
+        private val json = Json {
+            isLenient = true
+            ignoreUnknownKeys = true
         }
-    }
 
-    override fun toJsonString(obj: Any, type: Type): String {
-        return try {
-            val kType = type.toKType()
-            val serializer = json.serializersModule.serializer(kType)
-            json.encodeToString(serializer, obj)
-        } catch (e: SerializationException) {
-            throw Exception("Error while serializing JSON: ${e.message}")
-        } catch (e: IllegalStateException) {
-            throw Exception("Error state: ${e.message}")
-        }
-    }
-
-    private fun Type.toKType(): KType {
-        return when (this) {
-            is ParameterizedType -> {
-                val rawType = (this.rawType as Class<*>).kotlin
-                val args = this.actualTypeArguments.map { it.toKType() }
-                rawType.createType(args.map { KTypeProjection.invariant(it) })
+        override fun <T : Any> fromJsonString(jsonString: String, targetType: Type): T {
+            return try {
+                val kType : KType = targetType.toKType()
+                val serializer = json.serializersModule.serializer(kType)
+                json.decodeFromString(serializer, jsonString) as T
+            } catch (e: SerializationException) {
+                throw Exception("Error while deserializing JSON: ${e.message}")
+            } catch (e: IllegalStateException) {
+                throw Exception("Error state: ${e.message}")
             }
-            is Class<*> -> this.kotlin.createType()
-            else -> throw IllegalArgumentException("Unsupported Type: $this")
+        }
+
+        override fun toJsonString(obj: Any, type: Type): String {
+            return try {
+                val kType : KType = type.toKType()
+                val serializer = json.serializersModule.serializer(kType)
+                json.encodeToString(serializer, obj)
+            } catch (e: SerializationException) {
+                throw Exception("Error while serializing JSON: ${e.message}")
+            } catch (e: IllegalStateException) {
+                throw Exception("Error state: ${e.message}")
+            }
+        }
+
+        private fun Type.toKType(): KType {
+            return when (this) {
+                is ParameterizedType -> {
+                    val rawType = (this.rawType as Class<*>).kotlin
+                    val args = this.actualTypeArguments.map { it.toKType() }
+                    rawType.createType(args.map { KTypeProjection.invariant(it) })
+                }
+                is Class<*> -> this.kotlin.createType()
+                else -> throw IllegalArgumentException("Unsupported Type: $this")
+            }
         }
     }
-}
