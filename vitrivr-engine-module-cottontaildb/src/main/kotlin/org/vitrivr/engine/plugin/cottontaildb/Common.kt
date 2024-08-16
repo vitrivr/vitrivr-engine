7b79384d7c73e9ce2a22e788357128509af58825
Raphael <raphael.waltenspuel@unibas.ch>
2024-08-16 13:08:12 +0200

    maintains lc pg naming convention

--- vitrivr-engine-module-cottontaildb/src/main/kotlin/org/vitrivr/engine/plugin/cottontaildb/Common.kt
+++ vitrivr-engine-module-cottontaildb/src/main/kotlin/org/vitrivr/engine/plugin/cottontaildb/Common.kt
@@ -16,7 +16,7 @@ import java.util.*
 const val RETRIEVABLE_ENTITY_NAME = "retrievable"
 
 /** The column name of a retrievable ID. */
-const val RETRIEVABLE_ID_COLUMN_NAME = "retrievableId"
+const val RETRIEVABLE_ID_COLUMN_NAME = "retrievableid"
 
 /** The column name of a retrievable ID. */
 const val RETRIEVABLE_TYPE_COLUMN_NAME = "type"
@@ -25,10 +25,10 @@ const val RETRIEVABLE_TYPE_COLUMN_NAME = "type"
 const val RELATIONSHIP_ENTITY_NAME = "relationships"
 
 /** The column name of a retrievable ID. */
-const val SUBJECT_ID_COLUMN_NAME = "subjectId"
+const val SUBJECT_ID_COLUMN_NAME = "subjectid"
 
 /** The column name of a retrievable ID. */
-const val OBJECT_ID_COLUMN_NAME = "objectId"
+const val OBJECT_ID_COLUMN_NAME = "objectid"
 
 /** The column name of a retrievable ID. */
 const val PREDICATE_COLUMN_NAME = "predicate"
@@ -37,7 +37,7 @@ const val PREDICATE_COLUMN_NAME = "predicate"
 const val DESCRIPTOR_ENTITY_PREFIX = "descriptor"
 
 /** The column name of a descriptor ID. */
-const val DESCRIPTOR_ID_COLUMN_NAME = "descriptorId"
+const val DESCRIPTOR_ID_COLUMN_NAME = "descriptorid"
 
 /** The column name used to describe a distance.*/
 const val DISTANCE_COLUMN_NAME = "distance"
