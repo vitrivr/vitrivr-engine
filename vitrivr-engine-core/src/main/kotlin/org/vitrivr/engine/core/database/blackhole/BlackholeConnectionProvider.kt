e167816fcd10886afa823840ebc3dc6c2bc08067
Ralph Gasser <rg@pontius.ch>
2024-08-19 10:55:12 +0200

    Fixes issue in structure for BlackholeConnectionProvider.
    Signed-off-by: Ralph Gasser <rg@pontius.ch>

--- vitrivr-engine-core/src/main/kotlin/org/vitrivr/engine/core/database/blackhole/BlackholeConnectionProvider.kt
+++ vitrivr-engine-core/src/main/kotlin/org/vitrivr/engine/core/database/blackhole/BlackholeConnectionProvider.kt
@@ -12,7 +12,9 @@ import kotlin.reflect.KClass
  * @author Ralph Gasser
  * @version 1.0.0
  */
-class BlackholeConnectionProvider(override val databaseName: String, override val version: String = "1.0.0") : ConnectionProvider {
+class BlackholeConnectionProvider : ConnectionProvider {
+    override val databaseName: String = "blackhole"
+    override val version: String = "1.0.0"
     override fun openConnection(schemaName: String, parameters: Map<String, String>) = BlackholeConnection(schemaName, this, parameters["log"]?.toBoolean() == true)
     override fun <T : Descriptor<*>> register(descriptorClass: KClass<T>, provider: DescriptorProvider<*>) { /* No op. */ }
     override fun <T : Descriptor<*>> obtain(descriptorClass: KClass<T>) = BlackholeDescriptionProvider<T>()
