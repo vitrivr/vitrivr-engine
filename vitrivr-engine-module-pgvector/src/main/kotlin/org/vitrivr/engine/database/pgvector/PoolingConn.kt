1c6e5ede4914a6a6da01ecc306d6500caf90e143
Raphael <raphael.waltenspuel@unibas.ch>
2024-10-18 10:36:12 +0200

    removed singleton

--- vitrivr-engine-module-pgvector/src/main/kotlin/org/vitrivr/engine/database/pgvector/PoolingConn.kt
+++ vitrivr-engine-module-pgvector/src/main/kotlin/org/vitrivr/engine/database/pgvector/PoolingConn.kt
@@ -12,49 +12,29 @@ import java.util.*
  * @param properties `java.util.Properties` for the connection. Options are `user`, `password`.
 
  */
-class PoolingConn(url:String, properties: Properties) {
+class PoolingConn(url: String, properties: Properties) {
 
     private val config: HikariConfig = HikariConfig()
-    private lateinit var datasource: HikariDataSource
+    private  var datasource: HikariDataSource
 
     init {
-        if (instance == null) {
-            this.config.jdbcUrl = url
-            this.config.username = properties["user"]!!.toString()
-            this.config.password = properties["password"]!!.toString()
-            this.config.addDataSourceProperty("pool", "true")
-            this.config.addDataSourceProperty("prepStmtCacheSize", "250")
-            this.config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048")
-            this.datasource = HikariDataSource(config)
-            instance = this
-        }else{
-            throw SQLException("Connection pool has already been initialized!")
-        }
+        this.config.jdbcUrl = url
+        this.config.username = properties["user"]!!.toString()
+        this.config.password = properties["password"]!!.toString()
+        this.config.addDataSourceProperty("pool", "true")
+        this.config.addDataSourceProperty("prepStmtCacheSize", "250")
+        this.config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048")
+        this.datasource = HikariDataSource(config)
     }
 
     /***
      * Returns a connection for the connection pool.
      * @return Connection
+     * @throws SQLException If the connection pool has not been initialized.
      */
+    @get:Throws(SQLException::class)
     val connection: Connection
-        get() = datasource.connection
-
-    companion object {
-        @JvmStatic
-        private var instance: PoolingConn? = null
-
-        /***
-         * Returns a connection for the connection pool.
-         * @return Connection
-         * @throws SQLException If the connection pool has not been initialized.
-         */
-        @get:Throws(SQLException::class)
-        val connection: Connection
-            get() {
-                if (instance == null) {
-                    throw SQLException("Connection pool has not been initialized!")
-                }
-                return instance!!.datasource.connection
-            }
-    }
+        get() {
+            return datasource.connection
+        }
 }
