4b8292d97d7b9ed97afbab43d9cb07da7e8fea2d
Raphael <raphael.waltenspuel@unibas.ch>
2024-11-25 14:41:39 +0100

    adds late limit

--- vitrivr-engine-query/src/main/kotlin/org/vitrivr/engine/query/operators/transform/filter/LateFilterFactory.kt
+++ vitrivr-engine-query/src/main/kotlin/org/vitrivr/engine/query/operators/transform/filter/LateFilterFactory.kt
@@ -15,6 +15,7 @@ class LateFilterFactory() : TransformerFactory {
         val value = context[name, "value"] ?: throw IllegalArgumentException("expected 'value' to be defined in properties")
         val schemaField = context.schema[field] ?: throw IllegalArgumentException("Field '$field' not defined in schema")
         val append = context[name, "append"].toBoolean()
+        val limit = context[name, "limit"]?.toInt() ?: Int.MAX_VALUE
         val reader = schemaField.getReader()
         val providedKeys = context[name, "keys"]
         val keys = if(providedKeys?.isNotBlank() == true){
@@ -26,6 +27,6 @@ class LateFilterFactory() : TransformerFactory {
         }else{
             emptyList()
         }
-        return LateFilter(input, reader, keys, ComparisonOperator.fromString(comparison), value, append, name)
+        return LateFilter(input, reader, keys, ComparisonOperator.fromString(comparison), value, append, limit,  name)
     }
 }
