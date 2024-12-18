2007e0d4fae5b4ee0c4bb924ef1aefb7f78b6fd3
Raphael <raphael.waltenspuel@unibas.ch>
2024-12-18 16:32:45 +0100

    init for testing late filter

---
+++ vitrivr-engine-query/src/test/kotlin/org/vitrivr/engine/query/operators/transform/filter/LateFilterTest.kt
@@ -0,0 +1,33 @@
+package org.vitrivr.engine.query.operators.transform.filter
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.vitrivr.engine.core.context.Context
+import org.vitrivr.engine.core.context.QueryContext
+import org.vitrivr.engine.core.database.Initializer
+import org.vitrivr.engine.query.parsing.QueryParser
+
+class LateFilterTest {
+
+    var filter: LateFilter? = null
+
+    @BeforeEach
+    fun setUp() {
+        val context = QueryContext()
+        val retriever = MockRetrievedLookup()
+        this.filter = LateFilterFactory().newTransformer("filter", retriever, context)
+    }
+
+
+    @Test
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+
+}
+
