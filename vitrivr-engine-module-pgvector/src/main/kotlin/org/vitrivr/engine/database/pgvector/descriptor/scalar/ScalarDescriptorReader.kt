87c4fb3367dce0e9a6648e6c88916b8da1d75be7
Raphael <raphael.waltenspuel@unibas.ch>
2025-01-07 13:27:43 +0100

    Generalized Inverted Index

--- vitrivr-engine-module-pgvector/src/main/kotlin/org/vitrivr/engine/database/pgvector/descriptor/scalar/ScalarDescriptorReader.kt
+++ vitrivr-engine-module-pgvector/src/main/kotlin/org/vitrivr/engine/database/pgvector/descriptor/scalar/ScalarDescriptorReader.kt
@@ -2,6 +2,7 @@ package org.vitrivr.engine.database.pgvector.descriptor.scalar
 
 import org.vitrivr.engine.core.model.descriptor.scalar.*
 import org.vitrivr.engine.core.model.descriptor.scalar.ScalarDescriptor.Companion.VALUE_ATTRIBUTE_NAME
+import org.vitrivr.engine.core.model.descriptor.scalar.ScalarDescriptor.Companion.VALUE_INDEX_NAME
 import org.vitrivr.engine.core.model.descriptor.struct.StructDescriptor
 import org.vitrivr.engine.core.model.descriptor.vector.VectorDescriptor
 import org.vitrivr.engine.core.model.metamodel.Schema
@@ -12,6 +13,7 @@ import org.vitrivr.engine.core.model.retrievable.Retrieved
 import org.vitrivr.engine.core.model.types.Value
 import org.vitrivr.engine.database.pgvector.*
 import org.vitrivr.engine.database.pgvector.descriptor.AbstractDescriptorReader
+import org.vitrivr.engine.database.pgvector.descriptor.PgDescriptorInitializer
 import java.sql.ResultSet
 import java.util.*
 
@@ -21,7 +23,8 @@ import java.util.*
  * @author Ralph Gasser
  * @version 1.0.0
  */
-class ScalarDescriptorReader(field: Schema.Field<*, ScalarDescriptor<*, *>>, connection: PgVectorConnection) : AbstractDescriptorReader<ScalarDescriptor<*, *>>(field, connection) {
+class ScalarDescriptorReader(field: Schema.Field<*, ScalarDescriptor<*, *>>, connection: PgVectorConnection) :
+    AbstractDescriptorReader<ScalarDescriptor<*, *>>(field, connection) {
 
     /**
      * Executes the provided [Query] and returns a [Sequence] of [Retrieved]s that match it.
@@ -44,17 +47,19 @@ class ScalarDescriptorReader(field: Schema.Field<*, ScalarDescriptor<*, *>>, con
     override fun rowToDescriptor(result: ResultSet): ScalarDescriptor<*, *> {
         val descriptorId = result.getObject(DESCRIPTOR_ID_COLUMN_NAME, UUID::class.java)
         val retrievableId = result.getObject(RETRIEVABLE_ID_COLUMN_NAME, UUID::class.java)
+        // @formatter:off
         return when (this.prototype) {
-            is BooleanDescriptor -> BooleanDescriptor(descriptorId, retrievableId, Value.Boolean(result.getBoolean(VALUE_ATTRIBUTE_NAME)), this.field as Schema.Field<*, BooleanDescriptor>)
-            is ByteDescriptor -> ByteDescriptor(descriptorId, retrievableId, Value.Byte(result.getByte(VALUE_ATTRIBUTE_NAME)), this.field as Schema.Field<*, ByteDescriptor>)
-            is ShortDescriptor -> ShortDescriptor(descriptorId, retrievableId, Value.Short(result.getShort(VALUE_ATTRIBUTE_NAME)), this.field as Schema.Field<*, ShortDescriptor>)
-            is IntDescriptor -> IntDescriptor(descriptorId, retrievableId, Value.Int(result.getInt(VALUE_ATTRIBUTE_NAME)), this.field as Schema.Field<*, IntDescriptor>)
-            is LongDescriptor -> LongDescriptor(descriptorId, retrievableId, Value.Long(result.getLong(VALUE_ATTRIBUTE_NAME)), this.field as Schema.Field<*, LongDescriptor>)
-            is FloatDescriptor -> FloatDescriptor(descriptorId, retrievableId, Value.Float(result.getFloat(VALUE_ATTRIBUTE_NAME)), this.field as Schema.Field<*, FloatDescriptor>)
-            is DoubleDescriptor -> DoubleDescriptor(descriptorId, retrievableId, Value.Double(result.getDouble(VALUE_ATTRIBUTE_NAME)), this.field as Schema.Field<*, DoubleDescriptor>)
-            is StringDescriptor -> StringDescriptor(descriptorId, retrievableId, Value.String(result.getString(VALUE_ATTRIBUTE_NAME)), this.field as Schema.Field<*, StringDescriptor>)
-            is TextDescriptor -> TextDescriptor(descriptorId, retrievableId, Value.Text(result.getString(VALUE_ATTRIBUTE_NAME)),this.field as Schema.Field<*, TextDescriptor>)
+            is BooleanDescriptor -> BooleanDescriptor(descriptorId,retrievableId,Value.Boolean(result.getBoolean(VALUE_ATTRIBUTE_NAME)),this.field as Schema.Field<*, BooleanDescriptor>)
+            is ByteDescriptor -> ByteDescriptor(descriptorId,retrievableId,Value.Byte(result.getByte(VALUE_ATTRIBUTE_NAME)),this.field as Schema.Field<*, ByteDescriptor>)
+            is ShortDescriptor -> ShortDescriptor(descriptorId,retrievableId,Value.Short(result.getShort(VALUE_ATTRIBUTE_NAME)),this.field as Schema.Field<*, ShortDescriptor>)
+            is IntDescriptor -> IntDescriptor(descriptorId,retrievableId,Value.Int(result.getInt(VALUE_ATTRIBUTE_NAME)),this.field as Schema.Field<*, IntDescriptor>)
+            is LongDescriptor -> LongDescriptor(descriptorId,retrievableId,Value.Long(result.getLong(VALUE_ATTRIBUTE_NAME)),this.field as Schema.Field<*, LongDescriptor>)
+            is FloatDescriptor -> FloatDescriptor(descriptorId,retrievableId,Value.Float(result.getFloat(VALUE_ATTRIBUTE_NAME)),this.field as Schema.Field<*, FloatDescriptor>)
+            is DoubleDescriptor -> DoubleDescriptor(descriptorId,retrievableId,Value.Double(result.getDouble(VALUE_ATTRIBUTE_NAME)),this.field as Schema.Field<*, DoubleDescriptor>)
+            is StringDescriptor -> StringDescriptor(descriptorId,retrievableId,Value.String(result.getString(VALUE_ATTRIBUTE_NAME)),this.field as Schema.Field<*, StringDescriptor>)
+            is TextDescriptor -> TextDescriptor(descriptorId,retrievableId,Value.Text(result.getString(VALUE_ATTRIBUTE_NAME)),this.field as Schema.Field<*, TextDescriptor>)
         }
+        // @formatter:on
     }
 
     /**
@@ -64,8 +69,18 @@ class ScalarDescriptorReader(field: Schema.Field<*, ScalarDescriptor<*, *>>, con
      * @return [Sequence] of [ScalarDescriptor]s.
      */
     private fun queryFulltext(query: SimpleFulltextQuery): Sequence<ScalarDescriptor<*, *>> {
+
         val queryString = query.value.value.split(" ").map { "$it:*" }.joinToString(" | ") { it }
-        val statement = "SELECT * FROM \"${tableName.lowercase()}\" WHERE $VALUE_ATTRIBUTE_NAME @@ to_tsquery(?)"
+
+        val statement = this.field.indexes.firstOrNull() {
+            it.parameters["type"]?.lowercase() in PgDescriptorInitializer.INDEXES_FULLTEXT
+        }?.let {
+            when (it.parameters["type"]?.lowercase()) {
+                "gin" -> "SELECT * FROM \"${tableName.lowercase()}\" WHERE $VALUE_INDEX_NAME @@ to_tsquery('${it.parameters["language"] ?: "english"}', ?)"
+                else -> "SELECT * FROM \"${tableName.lowercase()}\" WHERE $VALUE_ATTRIBUTE_NAME @@ to_tsquery(?)"
+            }
+        }
+
         return sequence {
             this@ScalarDescriptorReader.connection.jdbc.prepareStatement(statement).use { stmt ->
                 stmt.setString(1, queryString)
