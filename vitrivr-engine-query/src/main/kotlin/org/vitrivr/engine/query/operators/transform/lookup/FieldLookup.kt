aa51885f260cc860ceecdfbbbdcee319c8a97447
Ralph Gasser <ralphgasser@me.com>
2025-01-30 10:56:47 +0100

    FieldLookup now makes sure, that descriptors are only contained once.

--- vitrivr-engine-query/src/main/kotlin/org/vitrivr/engine/query/operators/transform/lookup/FieldLookup.kt
+++ vitrivr-engine-query/src/main/kotlin/org/vitrivr/engine/query/operators/transform/lookup/FieldLookup.kt
@@ -8,8 +8,6 @@ import org.vitrivr.engine.core.database.descriptor.DescriptorReader
 import org.vitrivr.engine.core.model.metamodel.Schema
 import org.vitrivr.engine.core.model.retrievable.Retrievable
 import org.vitrivr.engine.core.model.retrievable.Retrieved
-import org.vitrivr.engine.core.model.retrievable.attributes.PropertyAttribute
-import org.vitrivr.engine.core.model.types.Value
 import org.vitrivr.engine.core.operators.Operator
 import org.vitrivr.engine.core.operators.general.Transformer
 import javax.management.Descriptor
@@ -17,14 +15,13 @@ import javax.management.Descriptor
 /**
  * Appends [Descriptor]s to a [Retrieved] based on the values of a [Schema.Field], if available.
  *
- * @version 1.1.3
+ * @version 1.2.0
  * @author Luca Rossetto
  * @author Ralph Gasser
  */
 class FieldLookup(
     override val input: Operator<out Retrievable>,
     private val reader: DescriptorReader<*>,
-    val keys: List<String>,
     override val name: String
 ) : Transformer {
     override fun toFlow(scope: CoroutineScope): Flow<Retrievable> = flow {
@@ -43,25 +40,9 @@ class FieldLookup(
         inputRetrieved.forEach { retrieved ->
             val descriptors = descriptorMap[retrieved.id] ?: emptySet()
             for (descriptor in descriptors) {
-                retrieved.addDescriptor(descriptor)
-
-                /* Somewhat experimental. Goal: Attach information in a meaningful manner, such that it can be serialised */
-                val values = descriptor.values().toMap()
-                retrieved.addAttribute(PropertyAttribute(keys.map{
-                    it to (when(values[it]){
-                        is Value.String -> (values[it] as Value.String).value
-                        is Value.Text -> (values[it] as Value.Text).value
-                        is Value.Boolean -> (values[it] as Value.Boolean).value
-                        is Value.Int -> (values[it] as Value.Int).value
-                        is Value.Long -> (values[it] as Value.Long).value
-                        is Value.Float -> (values[it] as Value.Float).value
-                        is Value.Double -> (values[it] as Value.Double).value
-                        is Value.Byte -> (values[it] as Value.Byte).value
-                        is Value.Short -> (values[it] as Value.Short).value
-                        is Value.DateTime -> (values[it] as Value.DateTime).value
-                        else -> values[it]
-                    }).toString()
-                }.toMap()))
+                if (retrieved.descriptors.none { it.id == descriptor.id }) {
+                    retrieved.addDescriptor(descriptor)
+                }
             }
             emit(retrieved)
         }
