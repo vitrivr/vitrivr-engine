bccbb4bcdf8cbcc3ea0f8ae1e89414e9a87ced11
Ralph Gasser <ralphgasser@me.com>
2025-03-18 22:06:14 +0100

    Correct erroneous changes.

--- vitrivr-engine-module-pgvector/src/main/kotlin/org/vitrivr/engine/database/pgvector/exposed/ops/DistanceOps.kt
+++ vitrivr-engine-module-pgvector/src/main/kotlin/org/vitrivr/engine/database/pgvector/exposed/ops/DistanceOps.kt
@@ -11,11 +11,7 @@ import org.jetbrains.exposed.sql.*
 sealed class DistanceOps(expr1: Expression<*>, expr2: Expression<*>, name: String) : CustomOperator<Float>(name, FloatColumnType(), expr1, expr2) {
     class Euclidean(expr1: Expression<*>, expr2: Expression<*>) : DistanceOps(expr1, expr2, "<->")
     class Manhattan(expr1: Expression<*>, expr2: Expression<*>) : DistanceOps(expr1, expr2, "<+>")
-    class Cosine(expr1: Expression<*>, expr2: Expression<*>) : DistanceOps(expr1, expr2, "<=>") {
-        override fun toQueryBuilder(queryBuilder: QueryBuilder): Unit = queryBuilder {
-            append('(', '1', ' ', '-', ' ', '(', expr1, ' ', operatorName, ' ', expr2, ')', ')')
-        }
-    }
+    class Cosine(expr1: Expression<*>, expr2: Expression<*>) : DistanceOps(expr1, expr2, "<=>")
     class Inner(expr1: Expression<*>, expr2: Expression<*>) : DistanceOps(expr1, expr2, "<#>")
     class Hamming(expr1: Expression<*>, expr2: Expression<*>) : DistanceOps(expr1, expr2, "<~>")
     class Jaccard(expr1: Expression<*>, expr2: Expression<*>) : DistanceOps(expr1, expr2, "<%>")
