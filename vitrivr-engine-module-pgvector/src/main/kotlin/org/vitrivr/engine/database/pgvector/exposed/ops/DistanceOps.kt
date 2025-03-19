d2b1cc5d28aab1d1a0deb00406191290d72a9752
Ralph Gasser <ralphgasser@me.com>
2025-03-19 12:13:58 +0100

    Fixes another compilation error.

--- vitrivr-engine-module-pgvector/src/main/kotlin/org/vitrivr/engine/database/pgvector/exposed/ops/DistanceOps.kt
+++ vitrivr-engine-module-pgvector/src/main/kotlin/org/vitrivr/engine/database/pgvector/exposed/ops/DistanceOps.kt
@@ -9,7 +9,7 @@ import org.jetbrains.exposed.sql.SqlExpressionBuilder.wrap
  * @author Ralph Gasser
  * @version 1.0.0
  */
-sealed class DistanceOps(expr1: Expression<*>, expr2: Expression<*>, name: String) : CustomOperator<Float>(name, FloatColumnType(), expr1, expr2) {
+sealed class DistanceOps(expr1: Expression<*>, expr2: Expression<*>, name: String) : CustomOperator<Double>(name, DoubleColumnType(), expr1, expr2) {
     class Euclidean(expr1: Expression<*>, expr2: Expression<*>) : DistanceOps(expr1, expr2, "<->")
     class Manhattan(expr1: Expression<*>, expr2: Expression<*>) : DistanceOps(expr1, expr2, "<+>")
     class Cosine(expr1: Expression<*>, expr2: Expression<*>) : DistanceOps(expr1, expr2, "<=>")
